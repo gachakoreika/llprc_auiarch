147d9bdb1c9d224fe2a4c1245ecf093778e2f287
gachak_ <126759852+gachakoreika@users.noreply.github.com>
2025-04-13 15:25:55 +0300

    Upload in_apps.py

---
+++ actions/installs/in_apps.py
@@ -0,0 +1,11 @@
+import os
+
+print("ЩАС ПРОЙДЁТ УСТАНОВКА ВАЖНЫХ ПРИЛОЖЕНИЙ!!!")
+print("ПОЖАЛЙСТА, НИЧЕГО НЕ ТРОГАЙТЕ И ПОДОЖДИТЕ, ПОКА ЭТО ВСЁ ПРОЙДЁТ!!!")
+print("Заранее спасибо)")
+
+os.system('sudo pacman -S krita telegram-desktop discord pcmanfm vim libreoffice-still libreoffice-still-ru htop')
+
+print("Важные пакеты установлены через pacman!")
+
+
